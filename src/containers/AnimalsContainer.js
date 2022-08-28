9ec50c9cb55a4da9083efa5de323915ab75c5d77
Scott Johnston <scott@umachan.co.uk>
2022-08-28 17:15:35 +0100

    Troubleshooting

--- src/containers/AnimalsContainer.js
+++ src/containers/AnimalsContainer.js
@@ -7,7 +7,6 @@ import Map from "../components/Map";
 import AnimalsService from "../services/AnimalsService";
 import AnimalDetail from "../components/AnimalDetail";
 import FunFactCard from '../components/FunFactCard';
-import HelloWorld from "../services/AnimalsService"
 
 const AnimalsContainer = () => {
 
@@ -25,19 +24,13 @@ const AnimalsContainer = () => {
 
     // Read in animals data from own api then load any favourites stored in localstorage
     useEffect(() => {
-        console.log("Getting data");
+        console.log("Getting animals");
         AnimalsService.getAnimals()
             .then((data) => {
-                console.log("DATA returned: ", data);
+                setAnimals(data)
+                setSelectedAnimal(data[0])
             })
-
-        // console.log("Getting animals");
-        // AnimalsService.getAnimals()
-        //     .then((data) => {
-        //         setAnimals(data)
-        //         setSelectedAnimal(data[0])
-        //     })
-        //     .then(getLocalStorageFavAnimals)
+            .then(getLocalStorageFavAnimals)
     }, [])
 
 
